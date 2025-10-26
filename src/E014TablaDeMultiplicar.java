6d01f5abe650e745c05d093086164475fcf1f094
k7054 <kamal.amarouch@educa.madrid.org>
2025-10-26 09:27:13 +0100

    .

---
+++ src/E014TablaDeMultiplicar.java
@@ -0,0 +1,19 @@
+void multiplicacion(int numero) {
+    for (int i = 1; i <= 10; i++) {
+        IO.println(String.format("%d x %d = %d", numero, i, numero * i));
+    }
+}
+
+void main() {
+    IO.println("Tabla de multiplicar");
+    String nombre = IO.readln("¿Cómo te llamas?\n").toUpperCase();
+
+    int num;
+
+    do {
+        num = Integer.parseInt(IO.readln(nombre + ", introduce un número entero positivo:\n"));
+    } while (num < 0);
+
+    IO.println();
+    multiplicacion(num);
+}
