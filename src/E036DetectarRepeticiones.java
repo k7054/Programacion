4254f216f07bf664ef3620a87818166b0098c488
k7054 <kamal.amarouch@educa.madrid.org>
2025-10-27 17:34:21 +0100

    .

--- src/E036DetectarRepeticiones.java
+++ src/E036DetectarRepeticiones.java
@@ -2,11 +2,11 @@ void main() {
     IO.println("DETECTAR REPETICIONES");
     String nombre = IO.readln("¿Cómo te llamas?\n").toUpperCase();
 
-    int num = Integer.parseInt(IO.readln(nombre + ", introduce un número\n"));
+    int num;
     int numSiguiente;
 
-    while (num < 0) {
+    do {
+        num = Integer.parseInt(IO.readln(nombre + ", introduce un número\n"));
         numSiguiente = Integer.parseInt(IO.readln(nombre + ", introduce otro número\n"));
-        num = numSiguiente;
-    }
+    } while (numSiguiente != num);
 }
