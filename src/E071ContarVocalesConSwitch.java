f7fb4fd51d1b29f13b68fb1cbfaeea3731bdea9b
k7054 <kamal.amarouch@educa.madrid.org>
2025-11-12 17:00:54 +0100

    .

---
+++ src/E071ContarVocalesConSwitch.java
@@ -0,0 +1,23 @@
+int contarVocales(String s) {
+    String sMinuscula = s.toLowerCase();
+
+    int contador = 0;
+
+    for (int i = 0; i < sMinuscula.length(); i++) {
+        char c = sMinuscula.charAt(i);
+        switch (c) {
+            case 'a', 'e', 'i', 'o', 'u' -> contador++;
+        }
+    }
+
+    return contador;
+}
+
+void main() {
+    IO.println("CONTAR VOCALES");
+    String nombre = IO.readln("¿Cómo te llamas?\n").toUpperCase();
+
+    String s = IO.readln(nombre + ", introduce una palabra o frase y te dire cuantas vocales tiene:\n");
+
+    IO.print(String.format("%s tiene %d vocales.\n", s, contarVocales(s)));
+}
