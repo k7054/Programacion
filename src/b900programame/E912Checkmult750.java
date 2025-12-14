9679631c8e9252fad3da9c6b5efbcfba0c336d47
k7054 <kamal.amarouch@educa.madrid.org>
2025-12-14 08:42:50 +0100

    K

---
+++ src/b900programame/E912Checkmult750.java
@@ -0,0 +1,34 @@
+package b900programame;
+
+import java.util.Scanner;
+
+public class E912Checkmult750 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        while (true) {
+            int num = entrada.nextInt();
+            if (num == 0) break;
+
+            int ceros = 0;
+            int multiplicacion;
+
+            while (num > 9) {
+                int digito;
+                multiplicacion = 1;
+                while (num > 0) {
+                    digito = num % 10;
+                    num = num / 10;
+                    if (digito == 0) {
+                        ceros++;
+                    } else {
+                        multiplicacion = multiplicacion * digito;
+                    }
+                }
+                num = multiplicacion;
+            }
+
+            System.out.printf("%d%d\n", num, ceros);
+        }
+    }
+}
