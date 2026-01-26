fac78506a30ca808e7c4c6e3eefea3835a68f747
k7054 <kamal.amarouch@educa.madrid.org>
2026-01-26 13:14:22 +0100

    K

---
+++ src/b800varios/E821FactorialRecursivo.java
@@ -0,0 +1,16 @@
+package b800varios;
+
+public class E821FactorialRecursivo {
+
+    public static int factorial(int numero) {
+        if (numero == 0){
+            return  1;
+        } else {
+            return numero * factorial(numero - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+    }
+}
