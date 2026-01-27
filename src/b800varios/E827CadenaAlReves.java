1e87a70837722614a75d13601dfac2910750ac1a
k7054 <kamal.amarouch@educa.madrid.org>
2026-01-27 12:33:03 +0100

    .

--- src/b800varios/E827CadenaAlReves.java
+++ src/b800varios/E827CadenaAlReves.java
@@ -1,4 +1,18 @@
 package b800varios;
 
 public class E827CadenaAlReves {
+
+    public static void cadenaAlReves(String cadena) {
+        if (cadena == null || cadena.length() == 0) {
+            return;
+        }
+
+        System.out.print(cadena.charAt(cadena.length() - 1));
+
+        cadenaAlReves(cadena.substring(0, cadena.length() - 1));
+    }
+
+    public static void main(String[] args) {
+        cadenaAlReves("ajos");
+    }
 }
